84031aae62643f57fd6fdf261c18021378a51eab
PFLeget <leget@stanford.edu>
2019-08-28 11:39:17 +0200

    two_pcf.py

---
+++ treegp/two_pcf.py
@@ -0,0 +1,192 @@
+import numpy as np
+import treecorr
+import copy
+from scipy import optimize
+
+class two_pcf(object):
+
+    def __init__(self, X, y, y_err,
+                 min_sep, max_sep, nbins=20, anisotropic=False):
+        """Fit statistical uncertaintie on two-point correlation function using bootstraping.
+
+        :param X:           The independent covariates.  (n_samples, 2)
+        :param y:           The dependent responses.  (n_samples, n_targets)
+        :param y_err:       Error of y. (n_samples, n_targets)
+        :param min_sep:     Minimum bin for treecorr. (float)
+        :param max_sep:     Maximum bin for treecorr. (float)
+        :param nbins:       Number of bins (if 1D correlation function) of the square root of the number
+                            of bins (if 2D correlation function) used in TreeCorr to compute the
+                            2-point correlation function. [default: 20]
+        :param anisotropic:  2D 2-point correlation function.
+                            Used 2D correlation function for the
+                            fiting part of the GP. (Boolean)
+        """
+        self.X = X
+        self.y = y
+        self.y_err = y_err
+        self.min_sep = min_sep
+        self.max_sep = max_sep
+        self.nbins = nbins
+        self.anisotropic = anisotropic
+
+    def resample_bootstrap(self):
+        """
+        Make a single bootstarp resampling on data.
+        """
+        npsfs = len(self.y)
+
+        ind_object = np.random.randint(0,npsfs-1, size=npsfs)
+        u_ressample = self.X[:,0][ind_object]
+        v_ressample = self.X[:,1][ind_object]
+        y_ressample = self.y[ind_object]
+        y_err_ressample = self.y_err[ind_object]
+
+        return u_ressample, v_ressample, y_ressample, y_err_ressample
+
+    def comp_2pcf(self, X, y, y_err):
+        """
+        Estimate 2-point correlation function using TreeCorr.
+
+        :param X:  The independent covariates.  (n_samples, 2)
+        :param y:  The dependent responses.  (n_samples, n_targets)
+        :param y_err: Error of y. (n_samples, n_targets)
+        """
+        if np.sum(y_err) == 0:
+            w = None
+        else:
+            w = 1./y_err**2
+
+        if self.anisotropic:
+            cat = treecorr.Catalog(x=X[:,0], y=X[:,1], k=(y-np.mean(y)), w=w)
+            kk = treecorr.KKCorrelation(min_sep=self.min_sep, max_sep=self.max_sep, nbins=self.nbins,
+                                        bin_type='TwoD', bin_slop=0)
+            kk.process(cat)
+            # Need a mask in the case of the 2D correlation function, to compute
+            # the covariance matrix using the bootstrap. The 2D correlation
+            # function is symmetric, so just half of the correlation function
+            # is useful to compute the covariance matrix. If it is not done,
+            # the covariance matrix is consequently not positive definite.
+            npixels = len(kk.xi)**2
+            mask = np.ones_like(kk.xi, dtype=bool)
+            mask = mask.reshape((int(np.sqrt(npixels)), int(np.sqrt(npixels))))
+
+            boolean_mask_odd = (len(mask)%2 == 0)
+            even_or_odd = len(mask)%2
+            nmask = int((len(mask)/2) + even_or_odd)
+            mask[nmask:,:] = False
+            mask[nmask-1][nmask:] = boolean_mask_odd
+
+            mask = mask.reshape(npixels)
+
+            distance = np.array([kk.dx.reshape(npixels), kk.dy.reshape(npixels)]).T
+            Coord = distance
+            xi = kk.xi.reshape(npixels)
+        else:
+            cat = treecorr.Catalog(x=X[:,0], y=X[:,1], k=(y-np.mean(y)), w=w)
+            kk = treecorr.KKCorrelation(min_sep=self.min_sep, max_sep=self.max_sep, nbins=self.nbins)
+            kk.process(cat)
+            distance = kk.meanr
+            mask = np.ones_like(kk.xi, dtype=bool)
+            Coord = np.array([distance,np.zeros_like(distance)]).T
+            xi = kk.xi
+
+        return xi, distance, Coord, mask
+
+    def comp_xi_covariance(self, n_bootstrap=1000, mask=None, seed=610639139):
+        """
+        Estimate 2-point correlation function covariance matrix using Bootstrap.
+
+        :param seed: seed of the random generator.
+        """
+        np.random.seed(seed)
+        xi_bootstrap = []
+        for i in range(n_bootstrap):
+            u, v, y, y_err = self.resample_bootstrap()
+            coord = np.array([u, v]).T
+            xi, d, c, m = self.comp_2pcf(coord, y, y_err)
+            if mask is None:
+                mask = np.array([True]*len(xi))
+            xi_bootstrap.append(xi[mask])
+        xi_bootstrap = np.array(xi_bootstrap)
+
+        dxi = xi_bootstrap - np.mean(xi_bootstrap, axis=0)
+        xi_cov = 1./(len(dxi)-1.) * np.dot(dxi.T, dxi)
+        return xi_cov
+
+    def return_2pcf(self, seed=610639139):
+        """
+        Return 2-point correlation function and its variance using Bootstrap.
+
+        :param seed: seed of the random generator.
+        """
+        xi, distance, coord, mask = self.comp_2pcf(self.X, self.y, self.y_err)
+        if self.anisotropic:
+            # Choice done from Andy Taylor et al. 2012
+            def f_bias(x, npixel=len(xi[mask])):
+                top = x - 1.
+                bottom = x - npixel - 2.
+                return (top/bottom) - 2.
+            results = optimize.fsolve(f_bias, len(xi[mask]) + 10)
+            xi_cov = self.comp_xi_covariance(n_bootstrap=int(results[0]), mask=mask, seed=seed)
+            bias_factor = (int(results[0]) - 1.) / (int(results[0]) - len(xi[mask]) - 2.)
+            xi_weight = np.linalg.inv(xi_cov) * bias_factor
+        else:
+            # let like developed initialy for the moment
+            xi_weight = np.eye(len(xi)) * 1./np.var(self.y)
+        return xi, xi_weight, distance, coord, mask
+
+    def optimizer(self, kernel):
+        """
+        Fit hyperparameter using two-point correlation function.
+        Used chi2 minimization with L-BFGS-B method from scipy.
+
+        :param kernel: sklearn.gaussian_process kernel.
+        """
+        size_x = np.max(self.X[:,0]) - np.min(self.X[:,0])
+        size_y = np.max(self.X[:,1]) - np.min(self.X[:,1])
+        rho = float(len(self.X[:,0])) / (size_x * size_y)
+        # if min_sep is None and isotropic GP, set min_sep to the average separation
+        # between data.
+        if self.min_sep is not None:
+            min_sep = self.min_sep
+        else:
+            if self.anisotropic:
+                min_sep = 0.
+            else:
+                min_sep = np.sqrt(1./rho)
+        # if max_sep is None, set max_sep to half of the size of the 
+        # given field.
+        if self.max_sep is not None:
+            max_sep = self.max_sep
+        else:
+            max_sep = np.sqrt(size_x**2 + size_y**2)/2.
+
+        xi, xi_weight, distance, coord, mask = self.return_2pcf()
+
+        def PCF(param, k=kernel):
+            kernel = k.clone_with_theta(param)
+            pcf = kernel.__call__(coord,Y=np.zeros_like(coord))[:,0]
+            return pcf
+
+        xi_mask = xi[mask]
+        def chi2(param):
+            residual = xi_mask - PCF(param)[mask]
+            return residual.dot(xi_weight.dot(residual))
+
+        p0 = kernel.theta
+
+        results_fmin = optimize.fmin(chi2,p0,disp=False)
+        results_bfgs = optimize.minimize(chi2,p0,method="L-BFGS-B")
+        results = [results_fmin, results_bfgs['x']]
+        chi2_min = [chi2(results[0]), chi2(results[1])]
+        ind_min = chi2_min.index(min(chi2_min))
+        results = results[ind_min]
+
+        self._2pcf = xi
+        self._2pcf_weight = xi_weight
+        self._2pcf_dist = distance
+        kernel = kernel.clone_with_theta(results)
+        self._2pcf_fit = PCF(kernel.theta)
+        self._2pcf_mask = mask
+        self._kernel = copy.deepcopy(kernel)
+        return kernel
