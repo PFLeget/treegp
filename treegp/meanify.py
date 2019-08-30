6ba373866e9277cf0e8b04709699d631c3648583
PFLeget <leget@stanford.edu>
2019-08-30 16:25:03 +0200

    done with reshaping meanify for treegp (not tested)

--- treegp/meanify.py
+++ treegp/meanify.py
@@ -1,141 +1,94 @@
 """
 .. module:: meanify
 """
-
-from __future__ import print_function
-
-import yaml
+import numpy as np
+from scipy.stats import binned_statistic_2d
+import fitsio
 import os
 
-def meanify(config):
-    """Take Piff output(s), build an average of the FoV, and write output average.
 
-    :param config:      The configuration file that defines how to build the model
+class meanify(object):
+    """Take data, build a spatial average, and write output average.
+
+    :param bin_spacing: Bin_size, resolution on the mean function. (default=120.)
+    :param statistics:  Statisitics used to compute the mean. (default=mean)
     """
-    from .star_temp import Star
-    import glob
-    import numpy as np
-    from scipy.stats import binned_statistic_2d
-    import fitsio
+    def __init__(self, bin_spacing=120., statistics='mean'):
 
-    for key in ['output', 'hyper']:
-        if key not in config:
-            raise ValueError("%s field is required in config dict"%key)
-    for key in ['file_name']:
-        if key not in config['output']:
-            raise ValueError("%s field is required in config dict output"%key)
+        self.bin_spacing = bin_spacing
 
-    for key in ['file_name']:
-        if key not in config['hyper']:
-            raise ValueError("%s field is required in config dict hyper"%key)
-
-    if 'dir' in config['output']:
-        dir = config['output']['dir']
-    else:
-        dir = None
-
-    if 'bin_spacing' in config['hyper']:
-        bin_spacing = config['hyper']['bin_spacing'] #in arcsec
-    else:
-        bin_spacing = 120. #default bin_spacing: 120 arcsec
-
-    if 'statistic' in config['hyper']:
-        if config['hyper']['statistic'] not in ['mean', 'median']:
+        if statistic not in ['mean', 'median']:
             raise ValueError("%s is not a suported statistic (only mean and median are currently suported)"
-                             %config['hyper']['statistic'])
-        else:
-            stat_used = config['hyper']['statistic']
-    else:
-        stat_used = 'mean' #default statistics: arithmetic mean over each bin
+                             %(statistics))
+        self.stat_used = statistics #default statistics: arithmetic mean over each bin
 
-    if 'params_fitted' in config['hyper']:
-        if type(config['hyper']['params_fitted']) != list:
-            raise TypeError('must give a list of index for params_fitted')
-        else:
-            params_fitted = config['hyper']['params_fitted']
-    else:
-        params_fitted = None
+        self.coords = []
+        self.params = []
 
-    if isinstance(config['output']['file_name'], list):
-        psf_list = config['output']['file_name']
-        if len(psf_list) == 0:
-            raise ValueError("file_name may not be an empty list")
-    elif isinstance(config['output']['file_name'], str):
-        file_name = config['output']['file_name']
-        if dir is not None:
-            file_name = os.path.join(dir, file_name)
-        psf_list = sorted(glob.glob(file_name))
-        if len(psf_list) == 0:
-            raise ValueError("No files found corresponding to "+config['file_name'])
-    elif not isinstance(config['file_name'], dict):
-        raise ValueError("file_name should be either a dict or a string")
+    def add_field(self, coord, param):
+        """
+        Add new data to compute the mean function. 
 
-    if psf_list is not None:
-        npsfs = len(psf_list)
-        config['output']['file_name'] = psf_list
+        :param coord: Array of coordinate of the parameter.
+        :param param: Array of parameter.
+        """
+        if np.shape(coord)[1] !=2:
+            raise ValueError('meanify is supported only in 2d for the moment.')
+        self.coords.append(coord)
+        self.params.append(param)
+    
+    def meanify(self):
+        """
+        Compute the mean function.
+        """
+        params = np.concatenate(self.params)
+        coords = np.concatenate(self.coords, axis=0)
 
-    file_name_in = config['output']['file_name']
+        lu_min, lu_max = np.min(coords[:,0]), np.max(coords[:,0])
+        lv_min, lv_max = np.min(coords[:,1]), np.max(coords[:,1])
 
-    file_name_out = config['hyper']['file_name']
-    if 'dir' in config['hyper']:
-        file_name_out = os.path.join(config['hyper']['dir'], file_name_out)
+        nbin_u = int((lu_max - lu_min) / bin_spacing)
+        nbin_v = int((lv_max - lv_min) / bin_spacing)
+        binning = [np.linspace(lu_min, lu_max, nbin_u), np.linspace(lv_min, lv_max, nbin_v)]
+        nbinning = (len(binning[0]) - 1) * (len(binning[1]) - 1)
+        Filter = np.array([True]*nbinning)
 
-    coords = []
-    params = []
+        average, u0, v0, bin_target = binned_statistic_2d(coords[:,0], coords[:,1],
+                                                          params, bins=binning,
+                                                          statistic=stat_used)
+        average = average.T
+        average = average.reshape(-1)
+        Filter &= np.isfinite(average).reshape(-1)
+        params0 = average
 
-    for fi, f in enumerate(file_name_in):
-        fits = fitsio.FITS(f)
-        coord, param = Star.read_coords_params(fits, 'psf_stars')
-        fits.close()
+        # get center of each bin 
+        u0 = u0[:-1] + (u0[1] - u0[0])/2.
+        v0 = v0[:-1] + (v0[1] - v0[0])/2.
+        u0, v0 = np.meshgrid(u0, v0)
 
-        coords.append(coord)
-        params.append(param)
+        coords0 = np.array([u0.reshape(-1), v0.reshape(-1)]).T
 
-    params = np.concatenate(params, axis=0)
-    coords = np.concatenate(coords, axis=0)
+        # remove any entries with nan (counts == 0 and non finite value in
+        # the 2D statistic computation) 
+        self.coords0 = coords0[Filter]
+        self.params0 = params0[Filter]
 
-    if params_fitted is None:
-        params_fitted = range(len(params[0]))
+    def save_results(dir='', name_output='mean_gp.fits'):
+        """
+        Write output mean function.
+        
+        :param dir:         Directory where to save the output file. (default: '')
+        :param name_output: Name of the output fits file. (default: 'mean_gp.fits')
+        """
+        file_name_out = os.path.join(rep, name_output)
 
-    lu_min, lu_max = np.min(coords[:,0]), np.max(coords[:,0])
-    lv_min, lv_max = np.min(coords[:,1]), np.max(coords[:,1])
+        dtypes = [('COORDS0', self.coords0.dtype, self.coords0.shape),
+                  ('PARAMS0', self.params0.dtype, self.params0.shape),
+                  ]
+        data = np.empty(1, dtype=dtypes)
+        
+        data['COORDS0'] = self.coords0
+        data['PARAMS0'] = self.params0
 
-    nbin_u = int((lu_max - lu_min) / bin_spacing)
-    nbin_v = int((lv_max - lv_min) / bin_spacing)
-    binning = [np.linspace(lu_min, lu_max, nbin_u), np.linspace(lv_min, lv_max, nbin_v)]
-    nbinning = (len(binning[0]) - 1) * (len(binning[1]) - 1)
-    params0 = np.zeros((nbinning, len(params[0])))
-    Filter = np.array([True]*nbinning)
-
-    for i in range(len(params[0])):
-        if i in params_fitted:
-            average, u0, v0, bin_target = binned_statistic_2d(coords[:,0], coords[:,1],
-                                                              params[:,i], bins=binning,
-                                                              statistic=stat_used)
-            average = average.T
-            average = average.reshape(-1)
-            Filter &= np.isfinite(average).reshape(-1)
-            params0[:,i] = average
-
-    # get center of each bin 
-    u0 = u0[:-1] + (u0[1] - u0[0])/2.
-    v0 = v0[:-1] + (v0[1] - v0[0])/2.
-    u0, v0 = np.meshgrid(u0, v0)
-
-    coords0 = np.array([u0.reshape(-1), v0.reshape(-1)]).T
-
-    # remove any entries with nan (counts == 0 and non finite value in
-    # the 2D statistic computation) 
-    coords0 = coords0[Filter]
-    params0 = params0[Filter]
-
-    dtypes = [('COORDS0', coords0.dtype, coords0.shape),
-              ('PARAMS0', params0.dtype, params0.shape),
-          ]
-    data = np.empty(1, dtype=dtypes)
-
-    data['COORDS0'] = coords0
-    data['PARAMS0'] = params0
-
-    with fitsio.FITS(file_name_out,'rw',clobber=True) as f:
-        f.write_table(data, extname='average_solution')
+        with fitsio.FITS(file_name_out,'rw',clobber=True) as f:
+            f.write_table(data, extname='average_solution')
