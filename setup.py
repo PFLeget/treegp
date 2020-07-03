2a07fe71ad268afb4d03c9cbac5b000c1b794d1e
Mike Jarvis <michael@jarvis.net>
2020-07-03 16:03:18 -0400

    Add version to setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,24 @@
 
 """Setup script."""
 
+from __future__ import print_function
 import glob
+import os
+import re
 from setuptools import setup, find_packages
+
+# Read in the version from treegp/_version.py
+# cf. http://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package
+version_file = os.path.join('treegp', '_version.py')
+with open(version_file, "rt") as f:
+    VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    txt = f.read()
+    mo = re.search(VSRE, txt, re.M)
+    if mo:
+        treegp_version = mo.group(1)
+    else:
+        raise RuntimeError("Unable to find version string in %s." % (version_file,))
+print("treegp version is %s" % (treegp_version))
 
 # Package name
 name = 'treegp'
@@ -21,5 +37,6 @@ setup(name=name,
       classifiers=["Topic :: Scientific :: Astronomy",
                    "Intended Audience :: Science/Research"],
       author="PFLeget",
+      version=treegp_version,
       packages=packages,
       scripts=scripts)
