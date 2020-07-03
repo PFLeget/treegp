c07547a0377d7c67dd65fe42f08a6f02a0c11a76
Mike Jarvis <michael@jarvis.net>
2020-07-03 16:04:30 -0400

    Add some more information in setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,18 @@
 
 from __future__ import print_function
 import glob
+import sys
 import os
 import re
+import setuptools
 from setuptools import setup, find_packages
+
+# Print some useful information in case there are problems, this info will help troubleshoot.
+print("Using setuptools version",setuptools.__version__)
+print("Python version = ",sys.version)
+
+with open('README.rst') as f:
+    long_description = f.read()
 
 # Read in the version from treegp/_version.py
 # cf. http://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package
@@ -24,19 +33,24 @@ print("treegp version is %s" % (treegp_version))
 # Package name
 name = 'treegp'
 
-# Packages (subdirectories in clusters/)
+# Packages
 packages = find_packages()
 
-# Scripts (in scripts/)
+# Scripts (none yet, but if we make any, they will live in scripts/)
 scripts = glob.glob("scripts/*.py")
 
 package_data = {}
 
 setup(name=name,
-      description=("treegp"),
+      description="treegp",
+      long_description=long_description,
+      license = "BSD License",
       classifiers=["Topic :: Scientific/Engineering :: Astronomy",
                    "Intended Audience :: Science/Research"],
       author="PFLeget",
+      author_email="plget [at] lpnhe.in2p3.fr",
+      url="https://github.com/PFLeget/treegp",
+      download_url="https://github.com/PFLeget/treegp/releases/tag/v%s.zip"%treegp_version,
       version=treegp_version,
       packages=packages,
       scripts=scripts)
