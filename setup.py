5127c8008380207ba7293ebd6d6382a61b1f1fb4
Mike Jarvis <michael@jarvis.net>
2020-12-10 11:43:28 -0500

    Limit iminuit to 1.x version

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ scripts = glob.glob("scripts/*.py")
 # Dependencies
 dependencies = ['numpy', 'scipy', 'treecorr>=4.0', 'fitsio>=0.9.12', 'scikit-learn>=0.18']
 if sys.version >= '3.0':
-    dependencies += ['iminuit']
+    dependencies += ['iminuit<2']
 else:
     # iminuit 1.4 fails on python 2.7
     dependencies += ['iminuit>=1.3,<1.4']
