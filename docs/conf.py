d4e6f2668bc14802d693251a1d0ed6691edb3ee5
PFLeget <pierrefrancois.leget@gmail.com>
2023-12-10 21:59:59 -0500

    v1.0.1

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ author = "Pierre-Francois Leget"
 # built documents.
 #
 # The short X.Y version.
-version = "1.0.0"
+version = "1.0.1"
 # The full version, including alpha/beta/rc tags.
 release = ""
 
